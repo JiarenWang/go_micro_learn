c8dabb1658ab0125d530ae4e6a781af8d40e2261
王佳仁 <wangjiaren@lvzhoutech.com>
2020-08-18 16:10:36 +0800

    commit

--- cmd/main.go
+++ cmd/main.go
@@ -11,12 +11,13 @@ import (
 	httptransport "github.com/go-kit/kit/transport/http"
 )
 
-// StringService ...
+// StringService 服务的定义
 type StringService interface {
 	hello(string) (string, error)
 	count(string) int
 }
 
+// 服务的具体实现者
 type stringService struct {
 }
 
@@ -31,6 +32,7 @@ func (stringService) count(string) int {
 	return 100
 }
 
+//服务数据的结构,为什么需要这个??因为rpc??
 type helloRequest struct {
 	S string `json:"s"`
 }
@@ -48,6 +50,10 @@ type countResponse struct {
 	V int `json:"v"`
 }
 
+//服务数据的结构,为什么需要这个??因为rpc??
+
+//相当于对服务的具体实现做了一层封装
+//端点??? 参数为实现服务的结构体,返回一个端点,端点是方法类型, 请求转化成结构体,从结构体获取请求数据
 func makeHelloEndpoint(svc StringService) endpoint.Endpoint {
 	return func(_ context.Context, request interface{}) (interface{}, error) {
 		req := request.(helloRequest)
@@ -59,6 +65,8 @@ func makeHelloEndpoint(svc StringService) endpoint.Endpoint {
 	}
 }
 
+//相当于对服务的具体实现做了一层封装
+//端点??? 参数为实现服务的结构体,返回一个端点,端点是方法类型, 请求转化成结构体,从结构体获取请求数据
 func makeCountEndpoint(svc StringService) endpoint.Endpoint {
 	return func(_ context.Context, request interface{}) (interface{}, error) {
 		req := request.(countRequest)
@@ -71,20 +79,23 @@ func makeCountEndpoint(svc StringService) endpoint.Endpoint {
 var ErrEmpty = errors.New("Empty string")
 
 func main() {
+	//服务实现者
 	svc := stringService{}
 
+	//某个服务的handel
 	uppercaseHandler := httptransport.NewServer(
 		makeHelloEndpoint(svc),
 		decodeUppercaseRequest,
 		encodeResponse,
 	)
 
+	//某个服务的handel
 	countHandler := httptransport.NewServer(
 		makeCountEndpoint(svc),
 		decodeCountRequest,
 		encodeResponse,
 	)
-
+	//路由注册
 	http.Handle("/hello", uppercaseHandler)
 	http.Handle("/count", countHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
